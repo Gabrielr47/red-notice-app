41c81f0964184b3a512a1c6de5f749bb7814be18
Gabriel Rodrigues <gabrielr47@gmail.com>
2022-03-30 10:23:50 +0200

    Remove search feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { SearchComponent } from './search/search.component';
 import { ListComponent } from './list/list.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HeaderComponent } from './header/header.component';
@@ -15,7 +14,6 @@ import { HeaderComponent } from './header/header.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    SearchComponent,
     ListComponent,
     ProfileComponent,
     HeaderComponent
